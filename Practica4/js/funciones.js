4d41cceb25d778678a7c382669872d930ab89cd3
unknown <g.era001@hotmail.com>
2015-03-04 16:15:17 -0600

    JQUERY and JavaScript

---
+++ Practica4/js/funciones.js
@@ -0,0 +1,10 @@
+/*function calcula(operacion){
+    var operador1 = document.calculadora.operador1.value;//obtener el valor de una caja de texto por medio de name
+    var operador2 = document.getElementById("operador2").value;//Obtener el valor de una caja de texto por medio del id
+    //alert(operador1);
+    //alert(operador2);
+    var result= eval(operador1 + operacion + operador2);//para evaluar una expresion matematica
+    document.calculadora.resultado.value= result;
+}*/
+
+alert("Hola JavaScript")
